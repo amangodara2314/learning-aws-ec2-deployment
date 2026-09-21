1f46617c3164759fecddcc9d681e6264233b7d8b
aman <amangodara5686@gmail.com>
2026-09-21 12:00:00 +0530

    learning docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node
+
+WORKDIR /node-app
+
+COPY package*.json .
+
+RUN npm install
+
+COPY . .
+
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
